c38e533d8c877b10e9f45a3b7a269a0c23ae6cd5
rendisagala <rendisagala19@gmail.com>
2022-01-20 19:49:34 +0700

    adding list

--- index.php
+++ index.php
@@ -9,5 +9,11 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere error excepturi minima incidunt adipisci, blanditiis commodi unde nemo aperiam quae?</p>
+<ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+</ul>
 </body>
 </html>
